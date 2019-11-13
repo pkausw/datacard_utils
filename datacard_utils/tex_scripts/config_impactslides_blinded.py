afb2a996d814bf5a66b701e76619599004be7be3
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2019-11-13 18:16:01 +0100

    included FH in combination
    Former-commit-id: a52214af44097e9d12c65731fe0f61e34b6a85c2

--- datacard_utils/tex_scripts/config_impactslides_blinded.py
+++ datacard_utils/tex_scripts/config_impactslides_blinded.py
@@ -15,133 +15,59 @@ slide_template = """\\subsection*{%(CLEARNAME)s}
 	"""
 
 prefix = "blinded_impacts"
-prefix_2016 = "2016"
 prefix_2017 = "2017"
-prefix_2016p2017 = "2016 + 2017"
 keyword_dict = {
-	"ttH_hbb_13TeV_2016": {
-		"label"		: prefix + "::2016_combination",
-		"clearname" : prefix_2016 +" Combined",
-	},
-	"ttH_hbb_13TeV_2016_fh":{
-		"label"		: prefix + "::fh_2016_combination",
-		"clearname"	: prefix_2016 + " FH"
-	},    
-	"ttH_hbb_13TeV_2016p2017_dl": {
-		"label"		: prefix + "::dl_2016p2017_combination",
-		"clearname"	: prefix_2016p2017 + " DL"
-	},
-	"ttH_hbb_13TeV_2017": {
+	"combined_DLFHSL_2017_baseline_v01": {
 		"label"		: prefix + "::2017_combination",
-		"clearname"	: prefix_2017 + " Combined"
+		"clearname"	: prefix_2017 + "\\\\ FH (DNN)+ DL (MEM) + SL (DNN) Combined"
 	},    
-	"ttH_hbb_13TeV_2017_fh": {
+	"combined_FH_2017_DNN": {
 		"label"		: prefix + "::fh_2017_combination",
 		"clearname"	: prefix_2017 + " FH"
 	},    
-	"ttH_hbb_13TeV_2017_sl_4j": {
-		"label"		: prefix + "::sl_2017_4j",
-		"clearname"	: prefix_2017 + " SL 4 Jets"
-	},    
-	"ttH_hbb_13TeV_2016_sl": {
-		"label"		: prefix + "::sl_2016_combination",
-		"clearname"	: prefix_2016 + " SL"
-	},    
-	"ttH_hbb_13TeV_2016p2017_fh": {
-		"label"		: prefix + "::fh_2016p2017_combination",
-		"clearname"	: prefix_2016p2017 + " FH"
-	},    
-	"ttH_hbb_13TeV_2017_dl": {
+	"combined_DL_2017_MEM": {
 		"label"		: prefix + "::dl_2017_combination",
 		"clearname"	: prefix_2017 + " DL"
 	},    
-	"ttH_hbb_13TeV_2017_fh_3b": {
-		"label"		: prefix + "::fh_2017_3b",
-		"clearname"	: prefix_2017 + " FH 3 b-tags"
-	},    
-	"ttH_hbb_13TeV_2017_sl_5j": {
-		"label"		: prefix + "::sl_2017_5j",
-		"clearname"	: prefix_2017 + " SL 5 Jets"
-	},    
-	"ttH_hbb_13TeV_2016_sldl": {
-		"label"		: prefix + "::sldl_2016_combination",
-		"clearname"	: prefix_2016 + " SL+DL"
-	},    
-	"ttH_hbb_13TeV_2016": {
-		"label"		: prefix + "::2016_combination",
-		"clearname"	: prefix_2016 + " Combined"
-	},    
-	"ttH_hbb_13TeV_2016p2017_sl": {
-		"label"		: prefix + "::sl_2016p2017_combination",
-		"clearname"	: prefix_2016p2017 + " SL"
-	},    
-	"ttH_hbb_13TeV_2017_dl_3j": {
-		"label"		: prefix + "::dl_2017_3j",
-		"clearname"	: prefix_2017 + " DL 3 Jets"
-	},    
-	"ttH_hbb_13TeV_2017_fh_4b": {
-		"label"		: prefix + "::fh_2017_4b",
-		"clearname"	: prefix_2017 + " FH 4 b-tags"
-	},    
-	"ttH_hbb_13TeV_2017_sl_6j": {
-		"label"		: prefix + "::sl_2017_6j",
-		"clearname"	: prefix_2017 + " SL 6 Jets"
-	},    
-	"ttH_hbb_13TeV_2016_dl": {
-		"label"		: prefix + "::dl_2016_combination",
-		"clearname"	: prefix_2016 + " DL"
-	},    
-	"ttH_hbb_13TeV_2016p2017": {
-		"label"		: prefix + "::2016p2017_combination",
-		"clearname"	: prefix_2016p2017 + " Combined"
-	},    
-	"ttH_hbb_13TeV_2016p2017_sldl": {
-		"label"		: prefix + "::sldl_2016p2017_combination",
-		"clearname"	: prefix_2016p2017 + " SL+DL"
-	},    
-	"ttH_hbb_13TeV_2017_dl_4j": {
-		"label"		: prefix + "::dl_2017_4j",
-		"clearname"	: prefix_2017 + " DL 4 Jets"
-	},    
-	"ttH_hbb_13TeV_2017_sl": {
+	"combined_SL_2017_DNN": {
 		"label"		: prefix + "::sl_2017_combination",
 		"clearname"	: prefix_2017 + " SL"
 	},    
-	"ttH_hbb_13TeV_2017_sldl": {
+	"combined_DLSL_2017_baseline_v01": {
 		"label"		: prefix + "::sldl_2017_combination",
 		"clearname"	: prefix_2017 + " SL+DL"
 	}
 }
 
-order = [
-	"ttH_hbb_13TeV_2017",
-	"ttH_hbb_13TeV_2017_sldl",
-	"ttH_hbb_13TeV_2017_sl",    
-	"ttH_hbb_13TeV_2017_dl",
-	"ttH_hbb_13TeV_2017_fh",
+#order = [
+#	"ttH_hbb_13TeV_2017",
+#	"ttH_hbb_13TeV_2017_sldl",
+#	"ttH_hbb_13TeV_2017_sl",    
+#	"ttH_hbb_13TeV_2017_dl",
+#	"ttH_hbb_13TeV_2017_fh",
 	
-	"ttH_hbb_13TeV_2017_sl_4j",
-	"ttH_hbb_13TeV_2017_sl_5j",
-	"ttH_hbb_13TeV_2017_sl_6j",    
-	"ttH_hbb_13TeV_2017_dl_3j",
-	"ttH_hbb_13TeV_2017_dl_4j",    
-	"ttH_hbb_13TeV_2017_fh_3b",
-	"ttH_hbb_13TeV_2017_fh_4b", 
+	#"ttH_hbb_13TeV_2017_sl_4j",
+	#"ttH_hbb_13TeV_2017_sl_5j",
+	#"ttH_hbb_13TeV_2017_sl_6j",    
+	#"ttH_hbb_13TeV_2017_dl_3j",
+	#"ttH_hbb_13TeV_2017_dl_4j",    
+	#"ttH_hbb_13TeV_2017_fh_3b",
+	#"ttH_hbb_13TeV_2017_fh_4b", 
 	
-	"ttH_hbb_13TeV_2016p2017",    
-	"ttH_hbb_13TeV_2016p2017_sldl",    
-	"ttH_hbb_13TeV_2016p2017_sl",   
-	"ttH_hbb_13TeV_2016p2017_dl",
-	"ttH_hbb_13TeV_2016p2017_fh",
+	#"ttH_hbb_13TeV_2016p2017",    
+	#"ttH_hbb_13TeV_2016p2017_sldl",    
+	#"ttH_hbb_13TeV_2016p2017_sl",   
+	#"ttH_hbb_13TeV_2016p2017_dl",
+	#"ttH_hbb_13TeV_2016p2017_fh",
 
-	"ttH_hbb_13TeV_2016",
-	"ttH_hbb_13TeV_2016_sldl",   
-	"ttH_hbb_13TeV_2016_sl",
-	"ttH_hbb_13TeV_2016_dl",    
-	"ttH_hbb_13TeV_2016_fh",
+	#"ttH_hbb_13TeV_2016",
+	#"ttH_hbb_13TeV_2016_sldl",   
+	#"ttH_hbb_13TeV_2016_sl",
+	#"ttH_hbb_13TeV_2016_dl",    
+	#"ttH_hbb_13TeV_2016_fh",
 	
-	]
-
+#	]
+order = keyword_dict.keys()
 
 logfile = "json_creation.log"
 
