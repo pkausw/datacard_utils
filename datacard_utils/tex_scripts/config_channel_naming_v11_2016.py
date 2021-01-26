f73f1f4747759068b081e20d8382a4f6b2c7174c
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-01-26 16:15:36 +0100

    configs for tex slides for 2016 combinations

---
+++ datacard_utils/tex_scripts/config_channel_naming_v11_2016.py
@@ -0,0 +1,182 @@
+keyword_dict = {
+    "combined_full_2016_baseline_v01": {
+		"label"		: "{}::2016_combination",
+		"clearname"	: "{}\\\\ FH (DNN)+ DL (DNN) + SL (DNN) \\\\ + ttbb CR Combined"
+	},
+    "combined_full_2016_baseline_v01_packaged_mus": {
+		"label"		: "{}::2016_combination_split",
+		"clearname"	: "{}\\\\ FH (DNN)+ DL (DNN) + SL (DNN) \\\\ + ttbb CR Split"
+	},
+    "combined_full_2016_baseline_v01_scalable": {
+		"label"		: "{}::2016_combination_scaled",
+		"clearname"	: "{}\\\\ FH (DNN)+ DL (DNN) + SL (DNN) \\\\ + ttbb CR Combined @ 140\\,fb-1"
+	},
+    "combined_full_2016_baseline_v01_scalable_packaged_mus": {
+		"label"		: "{}::2016_combination_scaled_split",
+		"clearname"	: "{}\\\\ FH (DNN)+ DL (DNN) + SL (DNN) \\\\ + ttbb CR Split @ 140\\,fb-1"
+	},
+    
+
+	"combined_DLFHSL_2016_baseline_v01": {
+		"label"		: "{}::dlslfh_2016_combination",
+		"clearname"	: "{}\\\\ FH (DNN)+ DL (DNN) + SL (DNN) Combined"
+	},
+    "combined_DLSL_2016_baseline_v01": {
+		"label"		: "{}::sldl_2016_combination",
+		"clearname"	: "{} SL+DL"
+	},
+	"combined_FH_2016_DNN": {
+		"label"		: "{}::fh_2016_combination",
+		"clearname"	: "{} FH"
+	},    
+	"combined_DL_2016_DNN": {
+		"label"		: "{}::dl_2016_combination",
+		"clearname"	: "{} DL"
+	},    
+    "combined_SL_2016_DNN": {
+		"label"		: "{}::sl_2016_combination",
+		"clearname"	: "{} SL"
+	},
+    "ttbb_CR_2016": {
+		"label"		: "{}::ttbb_CR_2016",
+		"clearname"	: "{} ttbb CR Split"
+	},
+
+    "ttH_hbb_13TeV_2016_dl_4j3b_MEM": {
+		"label"		: "{}::dl_4j3b_MEM_2016_combination",
+		"clearname"	: "{} DL $\\geq 4$ jets, 3 tags"
+	}, 
+    "ttH_hbb_13TeV_2016_dl_4j4b_MEM": {
+		"label"		: "{}::dl_4j4b_MEM_2016_combination",
+		"clearname"	: "{} DL $\\geq 4$ jets, $\\geq 4$ tags"
+	},    
+
+    "combined_SL_2016_DNN_ge4j_3t": {
+		"label"		: "{}::sl_ge4j_3t_2016_combination",
+		"clearname"	: "{} SL $\\geq 4$ jets, 3 tags"
+	}, 
+    "combined_SL_2016_DNN_ge4j_ge4t": {
+		"label"		: "{}::sl_ge4j_ge4t_2016_combination",
+		"clearname"	: "{} SL $\\geq 4$ jets, $\\geq 4$ tags"
+	},   
+
+    "fh_j7_t4_DNN_Node0": {
+		"label"		: "{}::fh_j7_t4_DNN_Node0_2016",
+		"clearname"	: "{} FH 7 jets, $\\geq 4$ tags"
+	},
+    "fh_j8_t4_DNN_Node0": {
+		"label"		: "{}::fh_j8_t4_DNN_Node0_2016",
+		"clearname"	: "{} FH 8 jets, $\\geq 4$ tags"
+	},
+    "fh_j9_t4_DNN_Node0": {
+		"label"		: "{}::fh_j9_t4_DNN_Node0_2016",
+		"clearname"	: "{} FH $\\geq 9$ jets, $\\geq 4$ tags"
+	},
+
+
+	
+    "combined_DLFHSL_2016_HT_jets":{
+		"label"		: "{}::dlslfh_2016_HT_jets",
+		"clearname"	: "{} DL+SL+FH HT(jets)"
+	},
+    "combined_DLSL_2016_HT_jets":{
+		"label"		: "{}::dlsl_2016_HT_jets",
+		"clearname"	: "{} DL+SL HT(jets)"
+	},
+    "combined_DL_2016_HT_jets.tex":{
+		"label"		: "{}::dl_2016_HT_jets",
+		"clearname"	: "{} DL HT(jets)"
+	},
+    "combined_SL_2016_HT_jets.tex":{
+		"label"		: "{}::sl_2016_HT_jets",
+		"clearname"	: "{} SL HT(jets)"
+	},
+    "combined_FH_2016_HT_jets.tex":{
+		"label"		: "{}::fh_2016_HT_jets",
+		"clearname"	: "{} FH HT(jets)"
+	},
+
+    "fh_j7_t4_ht30": {
+		"label"		: "{}::fh_j7_t4_ht30_2016",
+		"clearname"	: "{} FH 7 jets, $\\geq 4$ tags HT(jets)"
+	},
+    "fh_j8_t4_ht30": {
+		"label"		: "{}::fh_j8_t4_ht30_2016",
+		"clearname"	: "{} FH 8 jets, $\\geq 4$ tags HT(jets)"
+	},
+    "fh_j9_t4_ht30": {
+		"label"		: "{}::fh_j9_t4_ht30_2016",
+		"clearname"	: "{} FH $\\geq 9$ jets, $\\geq 4$ tags HT(jets)"
+	},
+
+    "ttH_hbb_13TeV_2016_dl_3j2b_HT_jets": {
+		"label"		: "{}::dl_3j2b_HT_jets_2016",
+		"clearname"	: "{} DL 3 jets, 2 tags"
+	}, 
+    "ttH_hbb_13TeV_2016_dl_3j3b_HT_jets": {
+		"label"		: "{}::dl_3j3b_HT_jets_2016",
+		"clearname"	: "{} DL 3 jets, 3 tags"
+	}, 
+    "ttH_hbb_13TeV_2016_dl_4j2b_HT_jets": {
+		"label"		: "{}::dl_4j2b_HT_jets_2016",
+		"clearname"	: "{} DL $\\geq 4$ jets, 2 tags"
+	}, 
+    "ttH_hbb_13TeV_2016_dl_4j3b_HT_jets": {
+		"label"		: "{}::dl_4j3b_HT_jets_2016",
+		"clearname"	: "{} DL $\\geq 4$ jets, 3 tags"
+	}, 
+    "ttH_hbb_13TeV_2016_dl_4j4b_HT_jets": {
+		"label"		: "{}::dl_4j4b_HT_jets_2016",
+		"clearname"	: "{} DL $\\geq 4$ jets, $\\geq 4$ tags"
+	}, 
+
+}
+newdict = {}
+for k in keyword_dict:
+    newkey = "No_minor_"+k
+    newdict[newkey] = keyword_dict[k]
+keyword_dict.update(newdict)
+
+order = [
+    "combined_full_2016_baseline_v01",
+    "combined_full_2016_baseline_v01_packaged_mus",
+    "combined_full_2016_baseline_v01_scalable",
+    "combined_full_2016_baseline_v01_scalable_packaged_mus",
+
+	"combined_DLFHSL_2016_baseline_v01",
+    "combined_DLSL_2016_baseline_v01",
+	"combined_FH_2016_DNN",    
+	"combined_DL_2016_DNN",    
+    "combined_SL_2016_DNN",
+    "ttbb_CR_2016",
+
+    # "ttH_hbb_13TeV_2016_dl_4j3b_MEM", 
+    # "ttH_hbb_13TeV_2016_dl_4j4b_MEM",    
+
+    "combined_SL_2016_DNN_ge4j_3t", 
+    "combined_SL_2016_DNN_ge4j_ge4t",   
+
+    "fh_j7_t4_DNN_Node0",
+    "fh_j8_t4_DNN_Node0",
+    "fh_j9_t4_DNN_Node0",
+
+
+	
+    "combined_DLFHSL_2016_HT_jets",
+    "combined_DLSL_2016_HT_jets",
+    "combined_DL_2016_HT_jets.tex",
+    "combined_SL_2016_HT_jets.tex",
+    "combined_FH_2016_HT_jets.tex",
+
+    "fh_j7_t4_ht30",
+    "fh_j8_t4_ht30",
+    "fh_j9_t4_ht30",
+
+    "ttH_hbb_13TeV_2016_dl_3j2b_HT_jets", 
+    "ttH_hbb_13TeV_2016_dl_3j3b_HT_jets", 
+    "ttH_hbb_13TeV_2016_dl_4j2b_HT_jets", 
+    "ttH_hbb_13TeV_2016_dl_4j3b_HT_jets", 
+    "ttH_hbb_13TeV_2016_dl_4j4b_HT_jets", 
+
+]
+order += ["No_minor_"+x for x in order]
