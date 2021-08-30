49c9d51bd59f88f96d3254056cc05de773864e64
Philip Daniel Keicher <philip.daniel.keicher@cern.ch>
2021-08-30 16:03:52 +0200

    adjusted workflow to read information from impact submition json

--- datacard_utils/impact_scripts/scripts/nll_scans_prepare_from_impacts.py
+++ datacard_utils/impact_scripts/scripts/nll_scans_prepare_from_impacts.py
@@ -2,16 +2,43 @@ import os, sys
 import glob
 import json
 
+this_dir = os.path.realpath(os.path.dirname(__file__))
+
+def load_parameters(indir, job_dic, logfile = "json_creation.log"):
+    params = []
+    lines = []
+    log_path = os.path.join(indir, logfile)
+    if not os.path.exists(log_path):
+        for e in glob.glob(os.path.join(indir, "*.err")):           
+            if os.stat(e).st_size!=0:
+                job_n = e.split(".")[-2]
+                param = job_dic[job_n]
+                params.append(param)
+    else:
+        with open(log_path) as f:
+            lines = f.read().splitlines()
+        
+        for l in lines:
+            l = l.strip()
+            if l.startswith("Missing"):
+                words = l.split()
+                params = words[-1].split(",")
+    for param in params:
+        print ("NLL to be run for parameter: "+param)
+    return params
 
 if __name__ == '__main__':
 
     current_dir = os.path.abspath(os.getcwd())
-    year = sys.argv[1]
-    print ("Year: " + year)
+    json_files = sys.argv[1:]
+    # print ("Year: " + year)
     
-    year_dir = os.path.join(current_dir, ("../"+year))
+    # year_dir = os.path.join(current_dir, ("../"+year))
     
-    for json_path in glob.glob(os.path.join(year_dir, "*.json")):
+    for json_path in json_files:
+        if not os.path.exists(json_path):
+            print("path '{}' does not exist".format(json_path))
+            continue
         with open(json_path) as f:
             dic = json.load(f)
         listOfDatacards = dic["datacards"]
@@ -20,50 +47,56 @@ if __name__ == '__main__':
 
         for d in listOfImpactFolders:
             print (d)
-            file_sh = open(glob.glob(os.path.join(d, "*.sh"))[0])
-            job_dic = {}
-            n=0
-            lines = file_sh.readlines()
-            for n in range(0, len(lines)):
-                if "-eq" in lines[n]:
-                    n_j = lines[n].split()[4]
-                    param_line = lines[n+1].split()
-                    match=0
-                    i=0
-                    for p in param_line:
-                        if p=="-P":
-                            match = i
-                            break
-                        i+=1
-                    param_j = param_line[match+1]
-                    job_dic[n_j] = param_j 
-            file_sh.close()
+            sh_files = glob.glob(os.path.join(d, "*.sh"))
+            if len(sh_files) == 0:
+                print("could not load any .sh files for '{}', skipping".format(d))
+                continue
+            elif len(sh_files) != 1:
+                msg = "found multiple .sh files for '{}', please check manually!"
+                raise ValueError(msg)
+                
+            with open(sh_files[0]) as file_sh:
+                job_dic = {}
+                n=0
+                lines = file_sh.readlines()
+                for n in range(0, len(lines)):
+                    if "-eq" in lines[n]:
+                        n_j = lines[n].split()[4]
+                        param_line = lines[n+1].split()
+                        match=0
+                        i=0
+                        for p in param_line:
+                            if p=="-P":
+                                match = i
+                                break
+                            i+=1
+                        param_j = param_line[match+1]
+                        job_dic[n_j] = param_j 
             
-            comb = d.split("/")[-1]
-            param_file = open("param_"+comb+".txt", "w+")
-            n_param = 0
-            for e in glob.glob(os.path.join(d, "*.err")):           
-                if os.stat(e).st_size!=0:
-                    job_n = e.split(".")[-2]
-                    param = job_dic[job_n]
-                    print ("NLL to be run for parameter: "+param)
-                    param_file.write(param+"\n")
-                    n_param+=1
-            param_file.close()
-            if n_param==0:
-                os.system("rm -rf param_"+comb+".txt")
-            datacard = ""
-            for card in listOfDatacards:
-                if comb in card:
-                    datacard = card
-                    break    
-            cmd = "python ../../../scripts/doNllscanFromTxt.py "
-            cmd += "param_"+comb+".txt "
-            cmd += datacard + " "
-            cmd += ' '.join(origSubmitCmds) + " "
-            cmd += "--robustFit 1"
-            print (cmd)
-            os.system(cmd)
+            par_list = load_parameters(indir = d, job_dic = job_dic)
+
+            if len(par_list) > 0:
+                comb = d.split("/")[-1]
+                param_file = "param_"+comb+".txt"
+
+                with open(param_file, "w") as f:
+                    f.write("\n".join(par_list))
+                
+                datacard = None
+                for card in listOfDatacards:
+                    if any("{}.{}".format(comb, ext) in card 
+                            for ext in "txt root"):
+                        datacard = card
+                        break  
+                if not datacard:
+                    raise ValueError("Could not find datacard for combination '{}'".format(comb))  
+                cmd = "python {}/doNllscanFromTxt.py ".format(this_dir)
+                cmd += "{} ".format(param_file)
+                cmd += datacard + " "
+                cmd += ' '.join(origSubmitCmds) + " "
+                cmd += "--robustFit 1"
+                print (cmd)
+                os.system(cmd)
         
         
         
