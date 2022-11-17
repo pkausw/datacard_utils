a4f1f021a109a30c0e7c3eddb3e89f07b38d2e62
Abhisek Datta <abhisek.datta@cern.ch>
2022-11-17 21:02:59 +0100

    update nll scripts

--- datacard_utils/impact_scripts/scripts/nll_merge_impact_calc.py
+++ datacard_utils/impact_scripts/scripts/nll_merge_impact_calc.py
@@ -272,7 +272,7 @@ if __name__ == '__main__':
         for poi in pois:
             
             cmd = "plotImpacts.py -i {infile} -o {name}_{poi} --POI {poi}".format(infile = impact_file, name = impact_name, poi = poi)
-            if not fit_type=="blind":
+            if fit_type=="blind":
                 cmd += " --blind"
             
             os.system(cmd)
